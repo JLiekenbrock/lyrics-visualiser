71ea0d87c221c07e8139e71fcdc4a0b942dad012
JLiekenbrock <jan.liekenbrock@protonmail.com>
2022-02-07 17:04:46 +0100

    update get_lyrics in gui

--- interfacetest.py
+++ interfacetest.py
@@ -2,19 +2,13 @@
 # visit http://127.0.0.1:8050/ in your web browser.
 
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash import dash, dcc, html
+from dash import dash, dcc, html, dash_table
 from dash.dependencies import Output, Input, State
-from dash.exceptions import PreventUpdate
 import json 
-import dash
-import dash_table
+
 from components import songsearch
 from components import nlp
 from components import visualisation
-
-import pandas as pd
 
 app = dash.Dash(__name__)
 server = app.server
@@ -27,7 +21,6 @@ app.layout = html.Div(children=[
     '''),
 
     html.Div([
-
         html.Label('Artist:'),
         dcc.Input(id='artist'),
 
@@ -50,26 +43,24 @@ app.layout = html.Div(children=[
     State("songtitle", "value"),
 )
 def get_lyrics(search,artist,songtitle):
-
     if artist is not None or "" and songtitle is not None or "":
         songtitle = songtitle
         artist = artist
         s = songsearch.find_song(artist, songtitle)
-        return json.dumps(s)
+        lyrics = nlp.clean_lyrics(s)
+        return json.dumps(lyrics)
     else:
         return json.dumps("no data here")
 
 @app.callback(Output('example-graph', 'figure'), Input('intermediate-value', 'data'))
 def update_graph(jsonified_cleaned_data):
     if jsonified_cleaned_data is not None:
-        lyrics = nlp.clean_lyrics(json.loads(jsonified_cleaned_data))
+        lyrics = json.loads(jsonified_cleaned_data)
         distances = nlp.distances(lyrics)    
-        figure=visualisation.heatmap(distances)
+        figure = visualisation.heatmap(distances)
         return figure
     else:
         return None
-    # more generally, this line would be
-    # json.loads(jsonified_cleaned_data        figure=visualisation.heatmap(distances)n figure
 
 if __name__ == '__main__':
     app.run_server(debug=True)
